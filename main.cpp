d5b90b6b2b287faf0dcaa57543c01434c93ad379
joshua kisb <joshuakisb@gmail.com>
2018-12-17 18:52:32 +0000

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -18,47 +18,5 @@ int main(int argc, char *argv[])
      // return app.exec();
 
     A2Config a2config;
-	//QTextStream out(stdout);
-
-	//  {
-	//  	std::vector<std::unique_ptr<Veck>> tree;
-
-	// 	// QStringList* ls1 = new QStringList({"a value", "another one"});
-	// 	// QStringList* ls2 = new QStringList({"a value", "another gu guy"});
-
-	// 	Veck* ls1 = new Veck("a value 1");
-	// 	Veck* ls2 = new Veck("a value 2");
-
-	// 	std::unique_ptr<Veck> lsPtr1{ls1};
-	// 	std::unique_ptr<Veck> lsPtr2{ls2};
-
-	// 	tree.emplace_back(std::move(lsPtr1));
-	// 	tree.emplace_back(std::move(lsPtr2));
-
-	// 	ls1->change("Now am three");
-	// 	// ls2->append("a third guy");
-
-	// 	// for (auto& ls : tree)
-	// 	// {
-	// 	// 	for (QString val: *ls)
-	// 	// 		out << "   [" << val << "]" << endl;
-	// 	// }
-	// }
-
-	//out << "out of the boop" << endl;
-
-	//ls2.append("language sir");
-
-	// QMapIterator<QString, QStringList*> iter(tree);
-
-	// while (iter.hasNext()) {
-	// 	iter.next();
-	// 	out << iter.key() << ": {" << endl;
-
-	// 	for (QString val: *(iter.value()))
-	// 		out << "   [" << val << "]" << endl;
-
-	// 	out << "}" << endl;
-	// }
 
 }
